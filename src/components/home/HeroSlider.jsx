19215efd5217fc4ca509bc50e764303b47dd9abc
Orkun Orhan <orkunorhan1996@gmail.com>
2026-07-02 20:24:07 +0300

    refactor: move mock data into data files
    - Create shared data directory
    - Move hero slide data into a separate file
    - Move product mock data into a separate file
    - Refactor components to consume external data

--- src/components/home/HeroSlider.jsx
+++ src/components/home/HeroSlider.jsx
@@ -1,26 +1,6 @@
 import Slider from "../common/Slider";
+import heroSlides from "../../data/heroSlides";
 import HeroSlide from "./HeroSlide";
-
-const heroSlides = [
-    {
-        id: 1,
-        image: "/images/hero-cover.jpg",
-        subtitle: "Summer 2020",
-        title: "New Collection",
-        description:
-            "We know how large objects will act, but things on a small scale.",
-        buttonText: "Shop Now",
-    },
-    {
-        id: 2,
-        image: "/images/hero-cover-2.jpg",
-        subtitle: "Summer 2020",
-        title: "New Collection",
-        description:
-            "We know how large objects will act, but things on a small scale.",
-        buttonText: "Shop Now",
-    },
-];
 
 function HeroSlider() {
     return (
---
+++ src/data/heroSlides.js
@@ -0,0 +1,22 @@
+const heroSlides = [
+    {
+        id: 1,
+        image: "/images/hero-cover.jpg",
+        subtitle: "Summer 2020",
+        title: "New Collection",
+        description:
+            "We know how large objects will act, but things on a small scale.",
+        buttonText: "Shop Now",
+    },
+    {
+        id: 2,
+        image: "/images/hero-cover-2.jpg",
+        subtitle: "Summer 2020",
+        title: "New Collection",
+        description:
+            "We know how large objects will act, but things on a small scale.",
+        buttonText: "Shop Now",
+    },
+];
+
+export default heroSlides;
