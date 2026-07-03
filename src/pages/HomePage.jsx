4aff3343dd385d5c3a99af19661ca3319816f716
Orkun Orhan <orkunorhan1996@gmail.com>
2026-07-03 18:46:45 +0300

    feat: implement product promo section
    - Create responsive product promotion section
    - Add promotional product image and marketing content
    - Implement call-to-action buttons
    - Match the Figma layout for mobile and desktop

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import HeroSlider from "../components/home/HeroSlider";
 import EditorsPick from "../components/home/EditorsPick";
 import BestsellerProducts from "../components/home/BestsellerProducts";
 import ProductPromoSlider from "../components/home/ProductPromoSlider";
+import ProductPromoSection from "../components/home/ProductPromoSection";
 
 function HomePage() {
     return (
@@ -10,6 +11,7 @@ function HomePage() {
             <EditorsPick />
             <BestsellerProducts />
             <ProductPromoSlider />
+            <ProductPromoSection />
         </>
     );
 }
