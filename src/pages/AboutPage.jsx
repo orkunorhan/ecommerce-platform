859a87a338af480045466dc353c671ed8803a07c
Orkun Orhan <orkunorhan1996@gmail.com>
2026-07-11 18:56:23 +0300

    feat: add team section to about page
    - Reuse the existing TeamMembers component on the About page
    - Match the Figma desktop and mobile layouts
    - Preserve the responsive TeamMembers component architecture

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import AboutHero from "../components/about/AboutHero";
 import AboutProblemSection from "../components/about/AboutProblemSection";
 import AboutStatistics from "../components/about/AboutStatistics";
 import AboutVideoSection from "../components/about/AboutVideoSection";
+import TeamMembers from "../components/team/TeamMembers";
 import aboutHero from "../data/aboutHero";
 import aboutProblem from "../data/aboutProblem";
 import aboutStatistics from "../data/aboutStatistics";
@@ -29,6 +30,7 @@ function AboutPage() {
                 poster={aboutVideo.poster}
                 title={aboutVideo.title}
             />
+            <TeamMembers />
         </>
     );
 }
